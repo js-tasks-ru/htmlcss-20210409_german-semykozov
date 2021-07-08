293ac00dfffd92e1af1e220d5280478d704cf7f3
german semikozov <course@javascript.info>
2021-07-08 19:27:42 +0300

    add btn

--- 09-lection7/01-burger-menu/burger-menu.js
+++ 09-lection7/01-burger-menu/burger-menu.js
@@ -1,19 +1,19 @@
-const menu = document.getElementById('burger-menu');
-const menuClose = document.getElementById('burger-menu-close');
-const menuOpen = document.getElementById('burger-menu-open');
+const menu = document.querySelector("#menu");
+const menuClose = document.querySelector("#burger-menu-close");
+const menuOpen = document.querySelector("#burger-menu-open");
 
 if (menuOpen) {
-    menuOpen.addEventListener('click', function() {
+    menuOpen.addEventListener("click", function() {
         if (menu) {
-            menu.setAttribute('data-open', '');
+            menu.setAttribute("data-open", true);
         }
     });
 }
 
 if (menuClose) {
-    menuClose.addEventListener('click', function() {
+    menuClose.addEventListener("click", function() {
         if (menu) {
-            menu.removeAttribute('data-open');
+            menu.removeAttribute("data-open", false);
         }
     });
 }
